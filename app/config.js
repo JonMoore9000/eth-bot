09a01478a2dca55ab19aea33feb3995b13d89bbe
JonMoore9000 <jonmoore9000@gmail.com>
2021-05-17 17:19:28 -0600

    Update config.js

--- app/config.js
+++ app/config.js
@@ -5,8 +5,8 @@
 // var config = require('./config.js');
 
 module.exports = {
-  consumer_key:         'D1EvvK19vLW8LqjHKtyLcvigB',
-  consumer_secret:      'fdkPjqeVzG2FUidZw8BU6mE4C2zuQEUVcAUAptf3pQDnEVveiU',
-  access_token:         'AAAAAAAAAAAAAAAAAAAAAMr8PgEAAAAA25%2BBErSF7gk3UeuOjSY3dc0EABg%3D5QnZG2vh0bR1MJLakfbrZwFF50OLrqPkvkfmA3kY24DFHrpURn',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
   access_token_secret:  ''
 }
